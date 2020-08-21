63d04b515802c3342dac610873177f49836ba1e8
Jonathan Carr <jonathan.carr@optimalworkshop.com>
2020-08-21 18:27:46 +1200

    Add react boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        Same age as my dog
+      </div>
+    </div>
+  );
+}
+
+export default App;

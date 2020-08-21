292fbc716b61b7d9f9704326cb4e20568f30946f
Jonathan Carr <jonathan.carr@optimalworkshop.com>
2020-08-21 18:40:24 +1200

    Add node-sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        Same age as my dog
-      </div>
+    <div className="container">
+      Same age as my dog
     </div>
   );
 }
